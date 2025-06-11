144c26e4b84d07f86d599239c9d3803e5ded9543
vel <vel@vels-MacBook-Air.local>
2025-06-11 22:07:51 +0530

    added initial funcationality calc

---
+++ src/src/Calc.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Calc {
+    public static void main(String[] args) {
+        add();
+    }
+
+    public static void add(){
+        System.out.println("Ading values");
+    }
+}
